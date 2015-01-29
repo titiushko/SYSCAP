273213481e95cfc05c05e9704c30127a2e134663
Tito Miguel <tito.javier.miguel.galdamez@gmail.com>
2015-01-29 15:14:22 -0600

    Importar SYSCAP con un click. Agregando la variable de entorno del directorio de SYSCAP en Linux.

--- database/importar-bd-syscap.sh
+++ database/importar-bd-syscap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mysql -h localhost -u root -p < /home/tito/www/syscap/database/syscap.sql
-mysql -h localhost -u root -p < /home/tito/www/syscap/database/syscap-funciones.sql
-mysql -h localhost -u root -p < /home/tito/www/syscap/database/syscap-vistas.sql
-mysql -h localhost -u root -p < /home/tito/www/syscap/database/syscap-datos.sql
+mysql -h localhost -u root -p < $SYSCAP/database/syscap.sql
+mysql -h localhost -u root -p < $SYSCAP/database/syscap-funciones.sql
+mysql -h localhost -u root -p < $SYSCAP/database/syscap-vistas.sql
+mysql -h localhost -u root -p < $SYSCAP/database/syscap-datos.sql
